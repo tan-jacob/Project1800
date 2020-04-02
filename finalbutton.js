19f4e316b6d916e65cf125b566d74d436dfe0319
hihihippo <[zhang.chris89@gmail.com]>
2020-04-01 22:38:48 -0700

    show final calc and past calcs

---
+++ finalbutton.js
@@ -0,0 +1,25 @@
+function addCount(item){
+        
+        var cal = localStorage.getItem(item)
+        console.log(cal);
+        console.log(item);
+        let count = parseInt(localStorage.getItem("total")) + parseInt(cal);
+        localStorage.setItem("total", count);
+        document.getElementById("whatever").innerHTML = count;
+    }   
+    //updates data for user 
+    function finishlogging(){
+        var finish = localStorage.getItem("total");
+        firebase.auth().onAuthStateChanged(function(user){
+            db.collection("users")
+            .doc(user.uid)
+            .update( {
+                "mytotal": finish
+            } )
+
+
+             })
+        document.getElementById("finishlog").innerHTML = finish;
+        localStorage.getItem("total", 0);
+
+    }
